c168ac8dee4e9480b719e16dd0203f968b9b7d65
Bilal Raja <bilraj@umich.edu>
2017-12-05 17:10:38 -0500

    Added conversion when highlighting off

--- content_script.js
+++ content_script.js
@@ -35,20 +35,13 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
         console.log("Sending DOM content")
 
         chrome.runtime.sendMessage({text: document.body.innerText}, 
+
         // Callback function should take array of dicts that need to be converted
         function(dict) {
             // Get all text nodes
             //var text_nodes = get_text_nodes()
 			conversions_dict = dict["dict"]
 
-
-            // // Test highlighting
-            // $('span').highlight('Bose');
-            // console.log("THE ELEMENT FOR BOSE IS: ")
-
-            // var c = $(arr[0]+":contains('0.20 kg')").attr("class");
-            // console.log("The class is: ")
-            // console.dir(c)
             var highlighting_enabled = false
             highlighting_enabled = localStorage.getItem("highlight");
 
@@ -60,13 +53,13 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
                     for(var val in conversions_dict[key]) {
 
                         if(highlighting_enabled) {
-                            //highlight(conversions_dict[key][val])
 
                             var spanClass = "highlight"
                             var replaceWith = "<mark> " + conversions_dict[key][val] + "</mark>"
                             document.body.innerHTML = document.body.innerHTML.replace(key, replaceWith)
                             console.log("The current key is: " + key)
-                            //$('body').text().highlight('Bose');
+                        } else {
+                            document.body.innerHTML = document.body.innerHTML.replace(key, conversions_dict[key][val])
                         }
 
                         //document.body.innerHTML = document.body.innerHTML.replace(key, conversions_dict[key][val])
